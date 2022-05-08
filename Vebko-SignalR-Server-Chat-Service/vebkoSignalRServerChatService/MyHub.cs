1a684d8ee7d7d3ecd21d5c86cf938775a6695df4
Taher <taherfattahi11@gmail.com>
2022-05-09 00:14:25 +0430

    add Vebko-SignalR-Server-Chat-Service

---
+++ Vebko-SignalR-Server-Chat-Service/vebkoSignalRServerChatService/MyHub.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vebkoSignalRChatService
+{
+    public class MyHub : Hub
+    {
+        //public void addMessage(string name, string message)
+        //{
+        //    Console.WriteLine($"Server send: {name} - {message}");
+        //    Clients.All.addMessage(name, message);
+
+        //    //Clients.User(Context.ConnectionId).addMessage(name, message);
+        //}
+
+        public async void addUserToMyHub(string userName)
+        {
+
+            Program.allUserDict.Add(Context.ConnectionId, userName);
+
+            Console.WriteLine($"Server send: {userName}");
+
+            //Clients.All.broadcast(userName, Context.ConnectionId, "111");
+
+            string allUserJson = JsonConvert.SerializeObject(Program.allUserDict);
+            await Program.client.EmitAsync("setAllOnlineUser", allUserJson);
+        }
+
+        public override Task OnConnected()
+        {
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
+            return base.OnConnected();
+        }
+
+        public async override Task OnDisconnected(bool stopCalled)
+        {
+            Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+
+            Program.allUserDict.Remove(Context.ConnectionId);
+
+            string allUserJson = JsonConvert.SerializeObject(Program.allUserDict);
+            await Program.client.EmitAsync("setAllOnlineUser", allUserJson);
+
+            base.OnDisconnected(stopCalled);
+        }
+
+        //public async Task Register(string username)
+        //{
+        //    var currentId = Context.ConnectionId;
+        //    if (!userLookup.ContainsKey(currentId))
+        //    {
+        //        // maintain a lookup of connectionId-to-username
+        //        userLookup.Add(currentId, username);
+        //        // re-use existing message for now
+        //        await Clients.AllExcept(currentId).SendAsync(
+        //            Messages.RECEIVE,
+        //            username, $"{username} joined the chat");
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Log connection
+        ///// </summary>
+        ///// <returns></returns>
+        //public override Task OnConnectedAsync()
+        //{
+        //    Console.WriteLine("Connected");
+        //    return base.OnConnectedAsync();
+        //}
+
+        ///// <summary>
+        ///// Log disconnection
+        ///// </summary>
+        ///// <param name="e"></param>
+        ///// <returns></returns>
+        //public override async Task OnDisconnectedAsync(Exception e)
+        //{
+        //    Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+        //    // try to get connection
+        //    string id = Context.ConnectionId;
+        //    if (!userLookup.TryGetValue(id, out string username))
+        //        username = "[unknown]";
+
+        //    userLookup.Remove(id);
+        //    await Clients.AllExcept(Context.ConnectionId).SendAsync(
+        //        Messages.RECEIVE,
+        //        username, $"{username} has left the chat");
+        //    await base.OnDisconnectedAsync(e);
+        //}
+
+    }
+}
---
+++ Vebko-SignalR-Server-Chat-Service/vebkoSignalRServerChatService/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Newtonsoft.Json;
+using Owin;
+using SocketIOClient;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace vebkoSignalRChatService
+{
+    public class Program
+    {
+        private const string Url = "http://127.0.0.1:9080";
+
+        public static IDictionary<string, string> allUserDict = new Dictionary<string, string>();
+
+        public static SocketIO client;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Server started");
+
+            client = new SocketIO("http://192.168.0.76:5002/");
+            //client = new SocketIO("http://127.0.0.1:5002/");
+
+            client.On("getAllOnlineUser", async response =>
+            {
+                string allUserJson = JsonConvert.SerializeObject(allUserDict);
+                await client.EmitAsync("setAllOnlineUserFromChatServer", allUserJson);
+            });
+
+
+            client.On("callingSocket", async response =>
+            {
+                var res = response.GetValue<string>();
+                string[] myRes = res.Split(new[] { "~!@#$" }, StringSplitOptions.None); // roomid - from - to
+
+                var context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+                context.Clients.All.broadcast(myRes[0], myRes[1], myRes[2]);
+            });
+
+            client.OnConnected += async (sender, e) =>
+            {
+                // Emit a string
+                //await client.EmitAsync("hi", "socket.io");
+
+                // Emit a string and an object
+                //var dto = new TestDTO { Id = 123, Name = "bob" };
+                //await client.EmitAsync("register", "source", dto);
+                await client.EmitAsync("setAdminSocket", "admin", client.Id);
+            };
+
+            using (WebApp.Start(Url, Configuration))
+            {
+                client.ConnectAsync().Wait();
+
+                Console.WriteLine($"Server running on {Url}");
+                Console.WriteLine("Press any key to send a message to connected clients");
+                Console.ReadKey();
+
+                // Send message to all connected clients
+                //var context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+                //context.Clients.All.addMessage("Server", "Hello from server");
+                //Console.ReadLine();
+            }
+
+        }
+
+        public static void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.MapSignalR();
+
+            //GlobalHost.Configuration.ConnectionTimeout = TimeSpan.FromSeconds(110);
+            GlobalHost.Configuration.DisconnectTimeout = TimeSpan.FromSeconds(6);
+            //GlobalHost.Configuration.KeepAlive = TimeSpan.FromSeconds(10);
+
+        }
+    }
+}
